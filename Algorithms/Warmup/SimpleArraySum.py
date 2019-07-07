1d58abb7be480c850d5456651bfef687971d9bc3
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 19:08:59 +0530

    Update SimpleArraySum.py

--- Algorithms/Warmup/SimpleArraySum.py
+++ Algorithms/Warmup/SimpleArraySum.py
@@ -3,16 +3,3 @@ def simpleArraySum(ar):
     for a in ar:
         sum = sum + a
     return sum
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = simpleArraySum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
