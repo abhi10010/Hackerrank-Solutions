1d353eea0f5d8e6b612886cee2f30af93cc2c8f5
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 19:18:34 +0530

    Create SockMerchant.py

---
+++ Algorithms/Implementation/SockMerchant.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+def sockMerchant(n, ar):
+    count = 0
+    ar.sort()
+    ar.append('#')
+    i = 0
+    while i<n:
+        if ar[i]==ar[i+1]:
+            count = count+1
+            i+=2
+        else:
+            i+=1
+    return count
