018259c5651c16696a2c0243cc069d5ac20e6927
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 17:51:37 +0530

    Create SimpleArraySum.py

---
+++ Algorithms/Warmup/SimpleArraySum.py
@@ -0,0 +1,18 @@
+def simpleArraySum(ar):
+    sum = 0
+    for a in ar:
+        sum = sum + a
+    return sum
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
