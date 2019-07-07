377d1c7f52017959d5b27928e3215a867ee4f8c1
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 18:59:04 +0530

    Create DiagonalDifference.py

---
+++ Algorithms/Warmup/DiagonalDifference.py
@@ -0,0 +1,38 @@
+import math
+import os
+import random
+import re
+import sys
+
+def diagonalDifference(arr):
+    d1 = 0
+    d2 = 0          
+    for r in range (0,len(arr)):
+        for c in range(0,len(arr)):
+            if r == c:
+                d1 += arr[r][c]
+            if r + c == len(arr)-1:
+                d2 += arr[r][c]
+    if d1>d2:
+        res = d1 - d2
+    if d2>d1:
+        res = d2 - d1
+    if d1==d2:
+        res = 0
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
