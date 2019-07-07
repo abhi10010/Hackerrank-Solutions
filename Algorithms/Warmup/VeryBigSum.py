a7968295b06f0ac13f349b64b06c2ac4c9b3733e
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 19:09:45 +0530

    Update VeryBigSum.py

--- Algorithms/Warmup/VeryBigSum.py
+++ Algorithms/Warmup/VeryBigSum.py
@@ -9,17 +9,3 @@ def aVeryBigSum(ar):
     for i in range(len(ar)):
         sum += ar[i]
     return sum
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    ar_count = int(input())
-
-    ar = list(map(int, input().rstrip().split()))
-
-    result = aVeryBigSum(ar)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
