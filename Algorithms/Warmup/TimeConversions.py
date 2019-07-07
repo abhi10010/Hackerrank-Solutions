e2b8f0016249bbc6aceda1772ffd315e23fa2e68
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 19:06:14 +0530

    Update TimeConversions.py

--- Algorithms/Warmup/TimeConversions.py
+++ Algorithms/Warmup/TimeConversions.py
@@ -1,1 +1,12 @@
+import os
+import sys
 
+def timeConversion(s):
+    if s[8:] == "AM" and s[0:2] == "12":
+        return "00"+s[2:8]
+    elif s[8:] == "AM":
+        return s[0:8]
+    elif s[8:] == "PM" and s[0:2]== "12":
+        return s[:8]
+    else:
+        return str(int(s[0:2])+12)+s[2:8]
