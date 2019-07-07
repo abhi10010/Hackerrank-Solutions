2a9417b49eb52e086cb15e61033640597d8c7824
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-07 19:01:40 +0530

    Update Staircase.py

--- Algorithms/Warmup/Staircase.py
+++ Algorithms/Warmup/Staircase.py
@@ -1,1 +1,21 @@
+import math
+import os
+import random
+import re
+import sys
+
+def staircase(n):
+    k = n-1
+    for i in range(0,n):
+        for j in range(0,k):
+            print(' ',end='')
+        k -= 1
+        for j in range(0,i+1):
+            print('#',end='')
+        print('\r')
+
+if __name__ == '__main__':
+    n = int(input())
+
+    staircase(n)
 
