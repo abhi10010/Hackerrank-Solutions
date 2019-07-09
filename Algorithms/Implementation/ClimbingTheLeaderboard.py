beb13efd69997c463bdd6b8e6ec2387f13b7084b
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-09 14:37:44 +0530

    Create ClimbingTheLeaderboard.py

---
+++ Algorithms/Implementation/ClimbingTheLeaderboard.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+def climbingLeaderboard(scores, alice):
+    
+    scores = sorted(list(set(scores)))
+    index = 0
+    rank_list = []
+    n = len(scores)
+    for i in alice:
+        while (n > index and i >= scores[index]):
+            index += 1
+        rank_list.append(n+1-index) 
+    return rank_list
