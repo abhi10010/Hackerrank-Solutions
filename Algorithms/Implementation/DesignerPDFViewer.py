0d6cc0aeab5be0092bc58740a06e4b9a3205d4d6
abhi10010 <52626190+abhi10010@users.noreply.github.com>
2019-07-10 13:15:00 +0530

    Update DesignerPDFViewer.py

--- Algorithms/Implementation/DesignerPDFViewer.py
+++ Algorithms/Implementation/DesignerPDFViewer.py
@@ -1,1 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
 
+def designerPdfViewer(h, word):
+    height=list()
+    char=list(word)
+    for i in range(len(char)):
+        char[i]=ord(char[i])
+        char[i]-=97
+    for i in char:
+        height.append(h[i])
+    max_height = max(height)
+    area = len(char)*max_height
+    return area
