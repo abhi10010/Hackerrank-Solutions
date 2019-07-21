4adc7f46d1ff9d99ffb7bd695ef6c85419ac42a0
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2019-07-21 19:18:04 +0530

    Update FlatlandSpaceStations.py

--- Algorithms/Implementation/FlatlandSpaceStations.py
+++ Algorithms/Implementation/FlatlandSpaceStations.py
@@ -4,10 +4,20 @@ import random
 import re
 import sys
 
-def flatlandSpaceStations(n, c):    
+def flatlandSpaceStations(n, c):
     a = list()
     if n==len(c):
         return 0
-    for i in range(n):
-            a.append(min((abs(i-c[k]) for k in range(len(c)))))
-    return max(a)
+    else:
+        if len(c)>1:
+            c.sort()
+            for i in range(len(c)-1):
+                a.append(int(abs(c[i]-c[i+1])/2))
+            a.append(c[0])
+            a.append(int(abs(c[-1]-n+1)))
+            print(a)
+            return max(a)
+        else:
+            for i in range(n):
+                a.append(int(abs(c[0]-i)))
+            return max(a)
