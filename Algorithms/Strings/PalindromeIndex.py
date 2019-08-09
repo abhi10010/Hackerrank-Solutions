f42aa8c3117a5dc5a3b97e9cc70f55db15e363b2
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2019-08-09 23:57:41 +0530

    Update PalindromeIndex.py

--- Algorithms/Strings/PalindromeIndex.py
+++ Algorithms/Strings/PalindromeIndex.py
@@ -8,7 +8,7 @@ def palindromeIndex(s):
     if s==s[::-1]:
         return -1
     else:
-        for i in range(int(len(s)/2)):
+        for i in range(len(s)//2):
             if s[i]!=s[len(s)-1-i]:
                 if (s[:i]+s[i+1:])==(s[:i]+s[i+1:])[::-1]:
                     return i
