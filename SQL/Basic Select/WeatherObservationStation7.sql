99d787ff38b5a2389b9e33aea6caf3b334fa218f
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-15 14:54:21 +0530

    Create WeatherObservationStation7.sql

---
+++ SQL/Basic Select/WeatherObservationStation7.sql
@@ -0,0 +1,10 @@
+SELECT
+    DISTINCT CITY
+FROM
+    STATION
+WHERE
+    RIGHT(CITY, 1) = 'A' 
+    OR RIGHT(CITY, 1) = 'E'
+    OR RIGHT(CITY, 1) = 'I'
+    OR RIGHT(CITY, 1) = 'O'
+    OR RIGHT(CITY, 1) = 'U'; 
