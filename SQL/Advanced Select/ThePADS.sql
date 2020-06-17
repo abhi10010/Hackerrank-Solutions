c8f95cdd35d555ebccfedbff662f3d3d79792bd4
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-17 20:41:17 +0530

    Create ThePADS.sql

---
+++ SQL/Advanced Select/ThePADS.sql
@@ -0,0 +1,14 @@
+SELECT
+    CONCAT_WS('', Name, '(',SUBSTR(Occupation, 1, 1), ')')
+FROM
+    OCCUPATIONS
+ORDER BY 
+    Name;
+SELECT
+    CONCAT_WS('','There are a total of ', COUNT(Occupation), ' ', LOWER(Occupation), 's.') 
+FROM
+    OCCUPATIONS
+GROUP BY
+    Occupation
+ORDER BY
+    COUNT(Occupation);
