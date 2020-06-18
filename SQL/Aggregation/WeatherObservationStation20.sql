d47ec6a81d897ae9ff845da79d1ec2e798e74479
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-06-18 13:38:25 +0530

    Update WeatherObservationStation20.sql

--- SQL/Aggregation/WeatherObservationStation20.sql
+++ SQL/Aggregation/WeatherObservationStation20.sql
@@ -1,1 +1,16 @@
-
+SELECT 
+    ROUND(S.LAT_N, 4)
+FROM
+    station S
+WHERE
+    (SELECT 
+        COUNT(Lat_N) 
+     FROM 
+        station 
+     WHERE 
+        Lat_N < S.LAT_N ) = (SELECT 
+                                COUNT(Lat_N) 
+                             FROM 
+                                station 
+                             WHERE 
+                                Lat_N > S.LAT_N)
