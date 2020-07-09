e817eba8026885aa14f6a961395aace624d15be7
Abhishek Rungta <52626190+abhi10010@users.noreply.github.com>
2020-07-09 11:48:16 +0530

    Create ValidatingPostalCodes.py

---
+++ Python/Regex and Parsing/ValidatingPostalCodes.py
@@ -0,0 +1,7 @@
+import re
+
+regex_integer_in_range = r"^[1-9]{1}\d{5}$"
+regex_alternating_repetitive_digit_pair = r"(\d)(?=\d\1)"	
+
+P = input()
+print (bool(re.match(regex_integer_in_range, P)) and len(re.findall(regex_alternating_repetitive_digit_pair, P)) < 2)
